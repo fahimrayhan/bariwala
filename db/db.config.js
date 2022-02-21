301474cd8aed2cfe4035ab1818c57613f0a20e27
Fahim Rayhan <fahimrayhan786@gmail.com>
2022-02-22 02:50:51 +0600

    DB Connected

---
+++ db/db.config.js
@@ -0,0 +1,23 @@
+import mysql from 'serverless-mysql'
+import 'dotenv/config'
+
+
+export const db = mysql({
+    config:{
+        host: process.env.HOST,
+        port: process.env.PORT,
+        database: process.env.DB,
+        user: process.env.USERNAME,
+        password: process.env.PASSWORD
+    }
+})
+
+export async function sql_query(query,values=[]) {
+    try {
+        const results = await db.query(query,values)
+        await db.end()
+        return results
+    } catch (error) {
+        console.error(error.message)
+    }
+}
--- pages/login.jsx
+++ pages/login.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 
 function login() {
   return (
@@ -6,27 +7,34 @@ function login() {
         <Head>
             <title>Login | Bariwala</title>
         </Head>
-        <body>
             <h1>Login</h1>
             <div>
-                  <form className="p-5">
+                  <form className="p-5 mx-auto" style={{maxWidth:'500px'}}>
                       <div className="mb-3">
                           <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required/>
                               <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                       </div>
                       <div className="mb-3">
                           <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                          <input type="password" className="form-control" id="exampleInputPassword1"/>
+                        <input type="password" className="form-control" id="exampleInputPassword1" required/>
                       </div>
                       <div className="mb-3 form-check">
                           <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
                           <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
                       </div>
-                      <button type="submit" className="btn btn-primary">Submit</button>
+                    <div className="text-center">
+                        <button type="submit" className="btn btn-primary text-center">Submit</button>
+                        <div className="mt-2">
+                            <p>Don't have an account? 
+                                <Link href="/register">
+                                    <a> Register</a>
+                                </Link>
+                            </p>
+                        </div>
+                    </div>
                   </form>
             </div>
-        </body>
     </>
   )
 }
